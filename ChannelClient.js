da2fd31492c7d3aeeff6401443941fce4252acc8
Gareth Jones <jones258@gmail.com>
2018-04-04 15:03:29 +1200

    added HMAC x-stamp-signature to the sendMessage request header, for security.

--- ChannelClient.js
+++ ChannelClient.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+const crypto = require('crypto');
 
 class ChannelClient {
     /**
@@ -43,6 +44,16 @@ class ChannelClient {
     }
 
     // region getters & setters
+    /**
+     *
+     * @return {'sha256'}
+     * @static
+     * @constant
+     */
+    static get HMAC_HEADER_SIGNATURE_ALGORITHM() {
+        return 'sha256';
+    }
+
     // region authToken (get & set)
     /**
      *
@@ -71,6 +82,55 @@ class ChannelClient {
 
     // endregion
 
+    // noinspection JSMethodCanBeStatic
+    /**
+     * Computes the HMAC-X signature for the given raw request body, using the given string.
+     *
+     * 'X' in this case is the algorithm, usually sha1 or sha256.
+     *
+     * The signature should be stored on the request as 'x-stamp-signature'
+     *
+     * @param {'sha1'|'sha256'|string} algorithm
+     * @param {string|Buffer} key
+     * @param {string} rawRequestBody
+     *
+     * @return {string} the computed signature of the raw request body, in uppercase.
+     * @protected
+     * @instance
+     */
+    _computeRequestSignature(algorithm, key, rawRequestBody) {
+        return crypto.createHmac(algorithm, key)
+                     .update(new Buffer(rawRequestBody))
+                     .digest('hex')
+                     .toUpperCase();
+    }
+
+    /**
+     * Builds the value for the x-stamp-signature header.
+     *
+     * @param {Object} requestBody
+     *
+     * @return {string}
+     * @private
+     */
+    _buildSignatureHeaderForRequest(requestBody) {
+        return this._computeRequestSignature(this.constructor.HMAC_HEADER_SIGNATURE_ALGORITHM, this._authToken, JSON.stringify(requestBody));
+    }
+
+    /**
+     *
+     * @param {Object} requestBody
+     *
+     * @return {{'X-StaMP-Signature': string, 'X-Clacks-Overhead': 'GNU Terry Pratchett'}}
+     * @private
+     */
+    _buildHeadersForRequest(requestBody) {
+        return {
+            'X-StaMP-Signature': this._buildSignatureHeaderForRequest(requestBody),
+            'X-Clacks-Overhead': 'GNU Terry Pratchett'
+        };
+    }
+
     /**
      * @param {string} senderId
      * @param {StaMP.Protocol.Letter} letter
@@ -78,7 +138,15 @@ class ChannelClient {
      * @return {AxiosPromise}
      */
     sendLetter(senderId, letter) {
-        return axios.post(this.fullZwermUrl, { token: this._authToken, senderId, letter });
+        const body = {
+            token: this._authToken,
+            senderId,
+            letter
+        };
+
+        const headers = this._buildHeadersForRequest(body);
+
+        return axios.post(this.fullZwermUrl, body, { headers });
     }
 }
 
